11a9ea9cd997753641207d76a58725ef65d63d99
Daniel Park <intheimageofyeshua@gmail.com>
2025-05-03 11:04:22 +0900

    Created Header Menu and Footer

--- 02-pancake-menu/src/index.js
+++ 02-pancake-menu/src/index.js
@@ -4,14 +4,44 @@ import ReactDOM from "react-dom/client";
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pancake Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>;
       <Pancake />
     </div>
   );
 }
 
+function Footer() {
+  return (
+    <footer>
+      {new Date().toLocaleTimeString()} We're currently closed! Please don't
+      ever come again!
+    </footer>
+  );
+  // return React.createElement("footer", null, "We're currently open!");
+}
+
 function Pancake() {
-  return <h2>Pancake</h2>;
+  return (
+    <div>
+      <img src="pancakes/pancake01.jpg" alt="Pancake Top" />
+      <h2>Pancake Top</h2>
+      <p>Tomato, and some good food ingredients</p>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
