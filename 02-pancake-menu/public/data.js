32267436779f019fd7fb8cd3141a60a77f507af9
Daniel Park <intheimageofyeshua@gmail.com>
2025-05-03 10:50:03 +0900

    Added Data and Pictures

---
+++ 02-pancake-menu/public/data.js
@@ -0,0 +1,44 @@
+const pancakeData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pancakes/pancake01.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pancake Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pancakes/pancake02.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pancake Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pancakes/pancake03.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pancake Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pancakes/pancake04.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pancake Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pancakes/pancake05.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pancake Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pancakes/pancake06.jpg",
+    soldOut: false,
+  },
+];
--- 02-pancake-menu/src/index.js
+++ 02-pancake-menu/src/index.js
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pancake />
+    </div>
+  );
+}
+
+function Pancake() {
+  return <h2>Pancake</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
